38d39ace2932223c8e63dd30ee3e34daf8eec029
Pochelyk <Pochelyk@mail.ru>
2017-08-11 10:29:54 +0300

    second

---
+++ Structurs3/Structurs3/Circle.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structurs3
+{
+    struct Circle
+    {
+        internal double R;
+        public double x;
+        public double y;
+        public double z;        
+        internal double AX;
+        internal double AY;
+        internal double AX1;
+        internal double AY1;
+        // площадь окружности
+        public double Area()
+        {
+            double S = Math.PI * R * R;
+            return S;
+        }
+        //площадь квадрата
+        public double AreaBox()
+        {
+            double S1 =  R * R;
+            return S1;
+        }
+        //центральный момент инерции для окружности
+        public double Inertial()
+        {
+            double J = (Math.PI * (Math.Pow(R * 2, 4)) / 64);
+            return J;
+        }
+        //момент инерции квадрата
+        public double InertialBox()
+        {
+            double J1 = AreaBox()* AreaBox()/12;
+            return J1;
+
+        }
+        //центральный момент инерции окружности оносительно х
+        public double InertialX()
+        {
+            double Jx = Inertial() + Area() * y * y;
+            return Jx;
+        }
+        //центральный момент инерции квадрата оносительно х
+        public double InertialX1()
+        {
+            double Jx1= Inertial() + AreaBox() * y * y;
+            return Jx1;
+        }
+        //центральный момент инерции окружности оносительно у
+        public double InertialY()
+        {
+            double Jx = Inertial() + Area() * x * x;
+            return Jx;
+        }
+        //центральный момент инерции квадрата оносительно у
+        public double InertialY1()
+        {
+            double Jx1= Inertial() + AreaBox() * y * y;
+            return Jx1;
+        }
+
+        public double CTX()
+        {
+            double CTX = (x * Area());
+            return CTX;
+        }
+        public double CTY()
+        {
+            double CTY = (y * Area());
+            return CTY;
+        }
+        //box
+        public double CTX1()
+        {
+            double CTX1 = (x * AreaBox());
+            return CTX1;
+        }
+        //box
+        public double CTY1()
+        {
+            double CTY1 = (y * AreaBox());
+            return CTY1;
+        }
+        //момент инерции по Х для каждой окружности относительно координаты общей тяжести
+        public double SecondX()
+        {
+            double SecondX = Inertial() + Area() * (AX - x) * (AX - x);
+            return SecondX;
+        }
+        //момент инерции по Х для квадрата относительно координаты общей тяжести
+        public double SecondX1()
+        {
+            double SecondX1 = InertialBox() + AreaBox() * (AX1 - x) * (AX1 - x);
+            return SecondX1;
+        }
+        //момент инерции по У для каждой окружности относительно координаты общей тяжести
+        public double SecondY()
+       {
+           double SecondY = Inertial() + Area() * (AY - y) * (AY - y);
+           return SecondY;
+        }
+        //момент инерции по У для квадрата относительно координаты общей тяжести
+        public double SecondY1()
+        {
+            double SecondY1 = InertialBox() + AreaBox() * (AY1 - y) * (AY1 - y);
+            return SecondY1;
+        }
+        //длина отрезка от координаты общей тяжести до центра окружности
+        public double FirstAB()
+        {
+           double AB = Math.Sqrt((AY - y) * (AY - y) + (AX - x) * (AX - x));
+           return AB;
+        }
+        //длина отрезка от координаты общей тяжести до центра квадрата
+        //public double FirstAB1()
+        //{
+       ///     double AB = Math.Sqrt((AY1 - y) * (AY1- y) + (AX1 - x) * (AX1- x));
+       //     return AB;
+       // }
+        // значение центра тяжести для каждой окружности относительно  координаты общей тяжести
+        public double FirstCT()
+        {
+           double CT = InertialX() + Area() * FirstAB() * FirstAB();
+           return CT;
+        }
+        // значение центра тяжести для каждого квадрата относительно  координаты общей тяжести
+        public double FirstCTbox()
+        {
+            double CT = InertialX1() + AreaBox() * FirstAB() * FirstAB();
+            return CT;
+        }
+        //circle
+        public double Function(double AX, double AY)
+        {
+            double AB = Math.Sqrt((AY - y) * (AY - y) + (AX - x) * (AX - x));
+            double CT = InertialX() + Area() *AB * AB;
+            return CT;
+        }
+        //box
+        public double FunctionBox(double AX1, double AY1)
+        {
+            double AB = Math.Sqrt((AY1 - y) * (AY1 - y) + (AX1 - x) * (AX1 - x));
+            double CT = InertialX1() + AreaBox() * AB * AB;
+            return CT;
+        }
+
+        public void Info()
+        {
+        }
+    }
+
+
+}
---
+++ Structurs3/Structurs3/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structurs3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество сечений:");
+            string circle = Console.ReadLine();
+            int R = Convert.ToInt32(circle);
+            double sum = 0;
+            double sum1 = 0;
+            double CentrX = 0;
+            double CentrY = 0;
+            double SumCircles = 0;
+            double SumBox = 0;
+
+            Circle[] circles = new Circle[R];
+            for (int i = 0; i < circles.Length; i++)
+            {
+                Console.WriteLine("Введите тип сечения К-квадрат, О-окружность:");
+                string size = Console.ReadLine();
+                switch (size)
+                {
+                    case "K":
+                        Console.WriteLine("Введите размер стороны квадрата:");
+                        string r1 = Console.ReadLine();
+                        Console.WriteLine("Введите координату X");
+                        string x1 = Console.ReadLine();
+                        Console.WriteLine("Введите координату у");
+                        string y1 = Console.ReadLine();
+                        Console.WriteLine("Введите координату z");
+                        string z1 = Console.ReadLine();
+                        double P1 = Convert.ToDouble(r1);
+                        double X1 = Convert.ToDouble(x1);
+                        double Y1 = Convert.ToDouble(y1);
+                        double Z1 = Convert.ToDouble(z1);
+                        circles[i].R = P1;
+                        circles[i].x = X1;
+                        circles[i].y = Y1;
+                        circles[i].z = Z1;                       
+                        SumBox += circles[i].AreaBox();
+                        CentrX += circles[i].CTX1();
+                        CentrY += circles[i].CTY1();
+                        circles[i].AX1 = +CentrX / +SumBox;
+                        circles[i].AY1 = +CentrY / +SumBox;
+                        Console.WriteLine("Площадь квадрата:{0}", circles[i].AreaBox());
+                        Console.WriteLine("Размер={0},координаты X={1},координаты У={2},координаты Z={3}", circles[i].R, circles[i].x, circles[i].y, circles[i].z);
+                        Console.WriteLine("Центральный момент инерции квадрата:{0}", circles[i].InertialBox());
+                        Console.WriteLine("Момент инерции относительно оси Х={0}, Момент инерции относительно оси У={1}", circles[i].InertialX1(), circles[i].InertialY1());
+                        Console.WriteLine("Централный момент инерции оносительно точки Ix={0}, Iy={1}", circles[i].SecondX1(), circles[i].SecondY1());
+                        Console.WriteLine("Централный момент инерции оносительно точки Ic={0}", circles[i].FirstCTbox());
+                        Console.WriteLine("Централный момент инерции оносительно точки Ic1={0}", circles[i].FunctionBox(+CentrX / +SumBox, +CentrY / +SumBox));
+                        Console.WriteLine("Координаты ЦТ X={0}, Y={1}", +CentrX / +SumBox, +CentrY / +SumBox);                       
+                        break;
+                    case "O":
+                        Console.WriteLine("Введите радиус окружности:");
+                        string r = Console.ReadLine();
+                        Console.WriteLine("Введите координату X");
+                        string x = Console.ReadLine();
+                        Console.WriteLine("Введите координату у");
+                        string y = Console.ReadLine();
+                        Console.WriteLine("Введите координату z");
+                        string z = Console.ReadLine();
+                        double P = Convert.ToDouble(r);
+                        double X = Convert.ToDouble(x);
+                        double Y = Convert.ToDouble(y);
+                        double Z = Convert.ToDouble(z);
+                        circles[i].R = P;
+                        circles[i].x = X;
+                        circles[i].y = Y;
+                        circles[i].z = Z;
+                        SumCircles += circles[i].Area();
+                        CentrX += circles[i].CTX();
+                        CentrY += circles[i].CTY();
+                        circles[i].AX = +CentrX / +SumCircles;
+                        circles[i].AY = +CentrY / +SumCircles;
+                        Console.WriteLine("Площадь окружности:{0}", circles[i].Area());
+                        Console.WriteLine("Радиус={0},координаты X={1},координаты У={2},координаты Z={3}", circles[i].R, circles[i].x, circles[i].y, circles[i].z);
+                        Console.WriteLine("Центральный момент инерции :{0}", circles[i].Inertial());
+                        Console.WriteLine("Момент инерции относительно оси Х={0}, Момент инерции относительно оси У={1}", circles[i].InertialX(), circles[i].InertialY());
+                        Console.WriteLine("Централный момент инерции оносительно точки Ix={0}, Iy={1}", circles[i].SecondX(), circles[i].SecondY());
+                        Console.WriteLine("Централный момент инерции оносительно точки Ic={0}", circles[i].FirstCT());
+                        Console.WriteLine("Централный момент инерции оносительно точки Ic1={0}", circles[i].Function(+CentrX / +SumCircles, +CentrY / +SumCircles));
+                        Console.WriteLine("Координаты ЦТ X={0}, Y={1}", +CentrX / +SumCircles, +CentrY / +SumCircles);                        
+                        break;
+                    default:
+                        Console.WriteLine("Вы нажали неизвестную букву");
+                        break;
+                }
+                sum += circles[i].Inertial();
+                sum1 += circles[i].InertialBox();
+            }
+
+            Console.WriteLine("Сумма моментов инерции окружностей:{0}", +sum);
+            Console.WriteLine("Сумма моментов инерции квадратов:{0}", +sum1);
+            Console.ReadKey();
+        }
+    }
+}
+
